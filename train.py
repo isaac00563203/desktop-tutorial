16984c0cd3d1363691138217a368c84506a2cbd9
isaac00563203 <isaac20230827@gmail.com>
2024-01-22 11:02:49 +0800

    readme.md

--- train.py
+++ train.py
@@ -7,12 +7,16 @@ from stable_baselines3 import DDPG
 import os
 
 symbol = "KQ.m@CFFEX.IF" # 合约代码, 天勤中的代码格式, 主力合约必须是 KQ.m@ 开头
-# 测试的数据周期：60秒（1分钟） 如果没有tq账户，可以用其他接口，代替该函数，数据格式 见 test.csv
+
+# 取测试的数据 如果没有tq账户，可以用其他接口，代替该函数，数据格式 见 .catche/test.csv
 data = getDataFromTqSdk(symbol, 60, "test.csv", datetime(2023, 11, 10, 9, 0, 0), datetime(2023, 11, 15, 15,0,0), False)
+
 # 加入技术指标
 data = add_tech_index(data, tech_list=tech_list, tech_para_list=tech_para_list)
+
 # 计算技术指标时，会产生一些Nan值，去掉
 data = data.iloc[200:]
+
 # 初始化期货 env
 env = FutureEnv(data, window_size=10)
 
